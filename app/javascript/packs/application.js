91c1f9234afa543fd1639c235b6947fa11aa927f
OneMoreGuy <boredomfighter12@gmail.com>
2022-02-10 14:16:23 +0200

    Fixed: CSS not displaying

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../bootstrap_js_files.js'
+import "../stylesheets/application.scss"
 
 Rails.start()
 Turbolinks.start()
