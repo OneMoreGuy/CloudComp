e148c3cf072ab26a5b769284e430d9d9d9598e4a
OneMoreGuy <boredomfighter12@gmail.com>
2022-02-11 21:38:24 +0200

    Upgraded to Bootstrap 5.0.1 to fix a bug with Modals.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("packs/style")
+//require("packs/style")
+//require("packs/modal")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../bootstrap_js_files.js'
-import "../stylesheets/application.scss"
+import '../stylesheets/application.scss'
+import './modal.js'
+import './style.js'
 
 Rails.start()
 Turbolinks.start()
