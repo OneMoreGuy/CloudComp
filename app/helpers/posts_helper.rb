cb47fb65b5e82a73f5113084d6d1f1097a211c2a
mikeboufas <michalis_vb@yahoo.com>
2022-02-13 18:41:45 +0200

    Inside a post add a form to contact a user - Define a contact_user_partial_path helper method in PostsHelper. - Create _contact_user.html.erb and _login_required.html.erb partials - Define a leave_message_partial_path helper method in PostsHelper. - Create _already_in_touch.html.erb and _message_form.html.erb   partial files - Define a @message_has_been_sent in PostsController's show action - Define a between_users scope inside the Private::Conversation model - Define private_conversation and private_message factories - Define routes for Private::Conversations and Private::Messages - Define a create action inside the Private::Conversations - Create _success.js and _fail.js partials

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -20,18 +20,32 @@ module PostsHelper
   def post_format_partial_path
     current_page?(root_path) ? '../views/posts/post/home_page' : '../views/posts/post/branch_page'
   end
-    def tag_field_partial_path
-      if params[:tag].present?
-        '../views/posts/branch/search_form/tag_field'
-      else
-        '../views/shared/empty_partial'
+  def tag_field_partial_path
+    if params[:tag].present?
+      '../views/posts/branch/search_form/tag_field'
+    else
+      '../views/shared/empty_partial'
     end
   end
   def update_pagination_partial_path
-  if @posts.next_page
-    'posts/posts_pagination_page/update_pagination'
-  else
-    'posts/posts_pagination_page/remove_pagination'
+    if @posts.next_page
+      'posts/posts_pagination_page/update_pagination'
+    else
+      'posts/posts_pagination_page/remove_pagination'
+    end
   end
-end
+  def contact_user_partial_path
+    if user_signed_in?
+      @post.user.id != current_user.id ? 'posts/show/contact_user' : 'shared/empty_partial'
+    else 
+      'posts/show/login_required'
+    end
+  end
+  def leave_message_partial_path
+    if @message_has_been_sent
+      'posts/show/contact_user/already_in_touch'
+    else
+      'posts/show/contact_user/message_form'
+    end
+  end
 end
