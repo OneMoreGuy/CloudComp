0a43b917f5c9e58f9d47b166407945785d486a89
OneMoreGuy <boredomfighter12@gmail.com>
2022-02-19 12:53:27 +0200

    Added services for group conversations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :opened_conversations_windows
   # Make conversations globally available.
   before_action :all_ordered_conversations
+  # Group conversations ids.
+  before_action :set_user_data
   
   
   def redirect_if_not_signed_in
@@ -16,10 +18,13 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       # opened conversations
       session[:private_conversations] ||= []
-      @private_conversations_windows = Private::Conversation.includes( :recipient, :messages)
-                                      .find(session[ :private_conversations])
+      session[:group_conversations] ||= []
+      @private_conversations_windows = Private::Conversation.includes(:recipient, :messages)
+                                           .find(session[:private_conversations])
+      @group_conversations_windows = Group::Conversation.find(session[:group_conversations])
     else
       @private_conversations_windows = []
+      @group_conversations_windows = []
     end
   end
   
@@ -27,6 +32,19 @@ class ApplicationController < ActionController::Base
   def all_ordered_conversations 
     if user_signed_in?
       @all_conversations = OrderConversationsService.new({user: current_user}).call
+    end
+  end
+  
+  private
+  
+  def set_user_data
+    if user_signed_in?
+      gon.group_conversations = current_user.group_conversations.ids
+      gon.user_id = current_user.id
+      cookies[:user_id] = current_user.id if current_user.present?
+      cookies[:group_conversations] = current_user.group_conversations.ids
+    else
+      gon.group_conversations = []
     end
   end
 end
